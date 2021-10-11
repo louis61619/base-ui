836faccd4f299522766ea717cf2943b6e9b321f8
louis61619 <louis61619@gmail.com>
2021-10-11 17:09:25 +0800

    feat(storybool): change storybook webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -5,5 +7,13 @@ module.exports = {
     '@storybook/addon-essentials',
     // '@storybook/preset-create-react-app',
     '@storybook/preset-scss'
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '..', 'src'),
+      path.resolve(__dirname, '..', 'node_modules')
+    ]
+
+    return config
+  }
 }
